6632422d0621529cbd4682f1b876a8f5fa3b43cf
Aygun <aygunaslanli023@gmail.com>
2024-11-25 16:55:04 +0400

    file added

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+import logoImg from "../assets/quiz-logo.png";
+
+
+
+export default function Header(){
+   
+    
+      return (
+        <header>
+        <img src={logoImg} />
+        <h1>ReactQuiz</h1>
+      </header>
+      )
+    }
